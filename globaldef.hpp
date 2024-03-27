d0be74cf30da8cea76c1bdc84f10ed0726a5cd6c
zhanghowe <zhanghowe122@gmail.com>
2024-03-27 15:43:50 +0800

    commit：修改托盘图标

--- globaldef.hpp
+++ globaldef.hpp
@@ -1,35 +1,32 @@
 ﻿#ifndef GLOBALDEF_HPP
 #define GLOBALDEF_HPP
-#include <QString>
 #include <QDebug>
 #include <QFile>
+#include <QString>
 
 #ifndef LOADQSS
-#define LOADQSS(qssFile)                        \
-{                                               \
-    QFile file(qssFile);                        \
-    file.open(QFile::ReadOnly);                 \
-    if(file.isOpen())                           \
-{                                           \
-    qApp->setStyleSheet(file.readAll());    \
-    file.close();                           \
-    }                                           \
-    }
+#    define LOADQSS(qssFile) \
+        { \
+            QFile file(qssFile); \
+            file.open(QFile::ReadOnly); \
+            if (file.isOpen()) { \
+                qApp->setStyleSheet(file.readAll()); \
+                file.close(); \
+            } \
+        }
 #endif
 
 #ifndef SAFEDELETE
-#define SAFEDELETE(pointer) \
-{                           \
-    if(pointer)             \
-{                       \
-    delete pointer;     \
-    }                       \
-    pointer = nullptr;          \
-    }
+#    define SAFEDELETE(pointer) \
+        { \
+            if (pointer) { \
+                delete pointer; \
+            } \
+            pointer = nullptr; \
+        }
 #endif
 
-enum WidgetTabType
-{
+enum WidgetTabType {
     TAB_BANNA,
     TAB_CYLINDER,
     TAB_PROGRESS,
@@ -44,45 +41,39 @@ enum WidgetTabType
     TAB_MAX,
 
 };
-namespace GlobalSpace
-{
-const QString STYLE_QSS_FILE_PATH = ":/res/res/stylesheet/stylesheet.qss";
-const QString LOGO_PATH = ":/res/res/image/image.png";
+namespace GlobalSpace {
+    const QString STYLE_QSS_FILE_PATH = ":/res/res/stylesheet/stylesheet.qss";
+    const QString LOGO_PATH = ":/res/res/image/icon.png";
 
-const int INT_DOUBLE_TIMES = 2;
-const float FLOAT_DOUBLE_TIMES = 2.0f;
+    const int INT_DOUBLE_TIMES = 2;
+    const float FLOAT_DOUBLE_TIMES = 2.0f;
 
-// 根据控件位置获取控件中心点位置
-inline QPoint GetCenterPosition(const QWidget *controlWidget)
-{
-    QPoint centerPos;
-    if(nullptr != controlWidget)
-    {
-        centerPos.setX(controlWidget->x() + controlWidget->width() / INT_DOUBLE_TIMES);
-        centerPos.setY(controlWidget->y() + controlWidget->height() / INT_DOUBLE_TIMES);
+    // 根据控件位置获取控件中心点位置
+    inline QPoint GetCenterPosition(const QWidget* controlWidget) {
+        QPoint centerPos;
+        if (nullptr != controlWidget) {
+            centerPos.setX(controlWidget->x() + controlWidget->width() / INT_DOUBLE_TIMES);
+            centerPos.setY(controlWidget->y() + controlWidget->height() / INT_DOUBLE_TIMES);
+        }
+        return centerPos;
     }
-    return centerPos;
-}
 
-// 获取控件中心点位置
-inline QPointF GetCenterPoint(const QWidget *controlWidget)
-{
-    QPointF centerPoint;
-    if(nullptr != controlWidget)
-    {
-        centerPoint.setX(controlWidget->width() / 2.0);
-        centerPoint.setY(controlWidget->height() / 2.0);
+    // 获取控件中心点位置
+    inline QPointF GetCenterPoint(const QWidget* controlWidget) {
+        QPointF centerPoint;
+        if (nullptr != controlWidget) {
+            centerPoint.setX(controlWidget->width() / 2.0);
+            centerPoint.setY(controlWidget->height() / 2.0);
+        }
+        return centerPoint;
     }
-    return centerPoint;
-}
 
-// 获取图片中心点位置
-inline QPointF GetCenterPoint(const QImage &image)
-{
-    QPointF centerPoint;
-    centerPoint.setX(image.width() / 2.0);
-    centerPoint.setY(image.height() / 2.0);
-    return centerPoint;
-}
-}
-#endif // GLOBALDEF_HPP
+    // 获取图片中心点位置
+    inline QPointF GetCenterPoint(const QImage& image) {
+        QPointF centerPoint;
+        centerPoint.setX(image.width() / 2.0);
+        centerPoint.setY(image.height() / 2.0);
+        return centerPoint;
+    }
+}  // namespace GlobalSpace
+#endif  // GLOBALDEF_HPP
--- systemtray/systemtray.cpp
+++ systemtray/systemtray.cpp
@@ -5,29 +5,25 @@
  * 功能，后续会继续优化
  * ***************************************/
 #include "systemtray.h"
-#include "globaldef.hpp"
+
 #include <QDebug>
+#include <QDesktopServices>
 #include <QIcon>
 #include <QUrl>
-#include <QDesktopServices>
+
+#include "globaldef.hpp"
 
 // 构造函数
-SystemTray::SystemTray(QWidget * parent)
-    :QSystemTrayIcon(parent)
-{
+SystemTray::SystemTray(QWidget* parent) : QSystemTrayIcon(parent) {
     this->createAction();  //创建托盘菜单
     this->addAction();     //菜单添加事件
 }
 
 // 析构函数
-SystemTray::~SystemTray()
-{
-
-}
+SystemTray::~SystemTray() {}
 
 // 添加菜单
-void SystemTray::addAction()
-{
+void SystemTray::addAction() {
     myMenu->addAction(showWidget);
     myMenu->addAction(minWidget);
     myMenu->addAction(aboutSoftWare);
@@ -35,8 +31,7 @@ void SystemTray::addAction()
 }
 
 // 创建菜单
-void SystemTray::createAction()
-{
+void SystemTray::createAction() {
     myMenu = new QMenu();
     showWidget = new QAction(tr("显示窗口 "), this);
     minWidget = new QAction(tr("隐藏窗口 "), this);
@@ -54,25 +49,13 @@ void SystemTray::createAction()
 }
 
 // 获取关于事件
-QAction *SystemTray::getAboutSoftWare() const
-{
-    return aboutSoftWare;
-}
+QAction* SystemTray::getAboutSoftWare() const { return aboutSoftWare; }
 
 // 关于软件
-void SystemTray::aboutSlot()
-{
-    QDesktopServices::openUrl(QUrl("https://github.com/YYC572652645/QCoolPage"));
-}
+void SystemTray::aboutSlot() { QDesktopServices::openUrl(QUrl("https://github.com")); }
 
 // 获取最小化事件
-QAction *SystemTray::getMinWidget() const
-{
-    return minWidget;
-}
+QAction* SystemTray::getMinWidget() const { return minWidget; }
 
 // 获取显示事件
-QAction *SystemTray::getShowWidget() const
-{
-    return showWidget;
-}
+QAction* SystemTray::getShowWidget() const { return showWidget; }
